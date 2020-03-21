ea470308a25434fbe58b9796382d3e06a97e25cc
Mark Ryan <ryanmark@rogers.com>
2020-03-21 15:38:58 -0400

    Mar 21b commit

--- deploy_web/flask_server.py
+++ deploy_web/flask_server.py
@@ -125,7 +125,7 @@ HTML_TEMPLATE = Template("""
 
 @app.route('/')
 def home():   
-
+    ''' render home page that is served at localhost and allows the user to enter details about their streetcar trip'''
     title_text = "Test title"
     title = {'titlename':title_text}
     return render_template('home.html',title=title)
@@ -134,11 +134,11 @@ def home():
 #@app.route('/test-link/')
 @app.route('/show-prediction/')
 def about():
-    print("I got tested")
-    # /test-link/?route=501&direction=e&year=2019&month=1&daym=1&day=6&hour=5
-    # score_cols = ['Route','Direction','hour','year','month','daym','day']
-    #
-    # load the parameter values into a dictionary indexed by the column names expected by the pipelines
+    ''' get the scoring parameters entered in home.html, assemble them into a dataframe, run that dataframe through pipelines
+        apply the trained model to the output of the pipeline, and display the interpreted score in show-prediction.html
+    '''
+    # the scoring parameters are sent to this page as parameters on the URL link from home.html
+    # load the scoring parameter values into a dictionary indexed by the column names expected by the pipelines
     score_values_dict = {}
     score_values_dict['Route'] = request.args.get('route')
     score_values_dict['Direction'] = request.args.get('direction')
@@ -150,38 +150,35 @@ def about():
     # echo the parameter values
     for value in score_values_dict:
         logging.warning("value for "+value+" is: "+str(score_values_dict[value]))
-    #logging.warning("url payload is route: "+route+",direction: "+direction+",year: "+year+",month: "+month+",daym: "+daym+",day: "+day+", hour: "+hour)
+    # load the trained model
     loaded_model = load_model(model_path)
     loaded_model._make_predict_function()
     logging.warning("DISPLAY PRED: model loaded")
+    # load the pipelines
     pipeline1 = load(open(pipeline1_path, 'rb'))
     pipeline2 = load(open(pipeline2_path, 'rb'))
     logging.warning("DISPLAY PRED: pipelines loaded")
-    # get prediction on canned value
-    # preds = loaded_model.predict(score_sample, batch_size=BATCH_SIZE)
-    score_current = {}
-    now = datetime.now()
-    # create and load dataframe that will be fed into pipeline
+    # create and load scoring parameters dataframe (containing the scoring parameters)that will be fed into the pipelines
     score_df = pd.DataFrame(columns=score_cols)
     logging.warning("score_df before load is "+str(score_df))
     for col in score_cols:
         score_df.at[0,col] = score_values_dict[col]
+    # apply the pipelines to the scoring parameters dataframe
     prepped_xform1 = pipeline1.transform(score_df)
     prepped_xform2 = pipeline2.transform(prepped_xform1)
-    print("prepped_xform2 is ",prepped_xform2)
+    logging.warning("prepped_xform2 is ",prepped_xform2)
+    # apply the trained model to the output of the pipelines to get a score
     pred = loaded_model.predict(prepped_xform2, batch_size=BATCH_SIZE)
     logging.warning("pred is "+str(pred))
+    # get a result string from the value of the score
     if pred[0][0] >= 0.5:
         predict_string = "yes, delay predicted"
     else:
         predict_string = "no delay predicted"
-    #logging.warning("pred is "+str(preds))
-    #logging.warning("preds[0] is "+str(preds[0]))
-    #logging.warning("preds[0][0] is "+str(preds[0][0]))
-    #print("here is a prediction "+str(preds[0][0]))
     prediction = {'prediction_key':predict_string}
+    # render the page that will show the prediction
     return(render_template('show-prediction.html',prediction=prediction))
-    # return "Delay prediction is: "+predict_string
+ 
 
 
 
