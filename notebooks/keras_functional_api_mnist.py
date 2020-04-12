5c2523c880e2856550bd57a7e333cf73ac8f3bc2
Mark Ryan <ryanmark@rogers.com>
2020-04-12 10:45:11 -0400

    Apr12b commit

--- notebooks/keras_functional_api_mnist.py
+++ notebooks/keras_functional_api_mnist.py
@@ -1,7 +1,14 @@
+# minimal Kera functional API model for MNIST
+# adapted from https://www.tensorflow.org/guide/keras/functional
+
+#import libraries
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+# define model layers
 
 inputs = keras.Input(shape=(784,)) 
 flatten = layers.Flatten(input_shape=(28, 28))
@@ -10,20 +17,28 @@ dense = layers.Dense(128, activation='relu')(flattened)
 dropout = layers.Dropout(0.2) (dense)
 outputs = layers.Dense(10) (dropout) 
 
+# define model inputs and outputs (taken from layer definition)
+
 model = keras.Model(inputs=inputs, outputs=outputs, name='mnist_model')
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 x_train = x_train.reshape(60000, 784).astype('float32') / 255 
 x_test = x_test.reshape(10000, 784).astype('float32') / 255
 
+# compile model, including specifying the loss function, optimizer, and metrics
+
 model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               optimizer=keras.optimizers.RMSprop(),
               metrics=['accuracy']) 
+
+# train model
 
 history = model.fit(x_train, y_train,
                     batch_size=64,
                     epochs=5,
                     validation_split=0.2) 
+
+# assess model performance
 
 test_scores = model.evaluate(x_test, y_test, verbose=2)
 print('Test loss:', test_scores[0])
--- notebooks/keras_sequential_api_mnist.py
+++ notebooks/keras_sequential_api_mnist.py
@@ -1,11 +1,20 @@
+# minimal Kera sequential API model for MNIST
+# adapted from https://github.com/tensorflow/docs/blob/master/site/en/tutorials/quickstart/beginner.ipynb
+
+#import libraries
+
 import tensorflow as tf
 import pydotplus
 from tensorflow.keras.utils import plot_model
 
 mnist = tf.keras.datasets.mnist
 
+# define model inputs
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# define model layers
 
 model = tf.keras.models.Sequential([ 
   tf.keras.layers.Flatten(input_shape=(28, 28)), 
@@ -14,16 +23,25 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10) 
 ])
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)#F
+# define loss function
+
+# loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+# compile model, including specifying the loss function, optimizer, and metrics
 
 model.compile(optimizer='adam',
-              loss=loss_fn,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy']) 
+
+# train model
 
 history = model.fit(x_train, y_train,
                     batch_size=64,
                     epochs=5,
-                    validation_split=0.2) 
+                    validation_split=0.2)
+
+# assess model performance
+                    
 test_scores = model.evaluate(x_test,  y_test, verbose=2) 
 print('Test loss:', test_scores[0])
 print('Test accuracy:', test_scores[1])
